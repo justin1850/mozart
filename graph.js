2d4adcedc471a37840fa0ab5afe839ac92aecced
DeveloperRB161 <battula26r@ncssm.edu>
2026-05-18 21:06:53 -0400

    JSDocs

--- graph.js
+++ graph.js
@@ -1,8 +1,19 @@
+/** 
+ * Represents/builds a weighted graph data structure based on an adjacency list
+ * 
+*/
 class Graph {
     constructor(){
         this.adjacency = {};
     }
 
+     /**
+     * Adds a directed edge from one node to another.
+     * Creates the source node in the graph if it does not already exist.
+     *
+     * @param {string} from - The source node.
+     * @param {string} to - The destination node.
+     */
     addEdge(from, to) {
         if (!this.adjacency[from]){
             this.adjacency[from] = [];
@@ -11,19 +22,39 @@ class Graph {
 
     }
 
+     /**
+     * Returns all neighbors of a given node.
+     *
+     * @param {string} node - The node to look up.
+     * @returns {string[]} Array of neighboring nodes, or an empty array if the node has none.
+     */
     getNeighbors(node){
         return this.adjacency[node] || [];
 
     }
 
+    /*
+     * Removes all nodes and edges from the graph.
+     */
     clear(){
         this.adjacency = {};
     }
 
+     /**
+     * Checks whether a node exists in the graph.
+     *
+     * @param {string} node - The node to check.
+     * @returns {boolean} True if the node exists, false otherwise.
+     */
     hasNode(node){
         return node in this.adjacency;
     }
 
+     /**
+     * Returns all nodes in the graph.
+     *
+     * @returns {string[]} Array of node identifiers.
+     */
     getNodes(){
         return Object.keys(this.adjacency);
     }
